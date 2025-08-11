bfe3cd6ac19c744d3b09444da613d0a102bb507e
sagarkumar16 <szkumar16@gmail.com>
2025-08-11 15:50:15 -0400

    removing autoconfig quantization

--- winogender_contextuality/modeling/run_local.py
+++ winogender_contextuality/modeling/run_local.py
@@ -1,6 +1,6 @@
 import torch
 from torch import bfloat16
-from transformers import AutoModelForCausalLM, BitsAndBytesConfig, PreTrainedModel, AutoTokenizer
+from transformers import AutoModelForCausalLM, BitsAndBytesConfig, PreTrainedModel, AutoTokenizer, AutoConfig
 import huggingface_hub
 from winogender_contextuality.config import *
 
@@ -35,8 +35,7 @@ def load_model(model_name: str,
     if not quantized:
         # full model
         logger.info('Loading full model', flush=True)
-        model = AutoModelForCausalLM.from_pretrained(model_name, token=api_key, cache_dir=cache_dir)
-        model = model.to(GPU_INDEX)
+        model = AutoModelForCausalLM.from_pretrained(model_name, token=api_key, cache_dir=cache_dir, device_map="auto")
         model.config.use_cache = False
     else:
         # quantized version
@@ -46,6 +45,11 @@ def load_model(model_name: str,
             bnb_4bit_use_double_quant=True,
             bnb_4bit_compute_dtype=torch.bfloat16,  # note: use torch.bfloat16
         )
+
+        cfg = AutoConfig.from_pretrained(model_name, cache_dir=cache_dir)
+        # remove model-advertised quantization (e.g., MXFP4)
+        if hasattr(cfg, "quantization_config"):
+            setattr(cfg, "quantization_config", None)
 
         tok = AutoTokenizer.from_pretrained(model_name)
         model = AutoModelForCausalLM.from_pretrained(
