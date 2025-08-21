f68e8f80db6896a867caf13f8f4863c476695d3b
sagarkumar16 <szkumar16@gmail.com>
2025-08-20 20:06:30 -0400

    testing

--- winogender_contextuality/modeling/collect_sequential.py
+++ winogender_contextuality/modeling/collect_sequential.py
@@ -229,6 +229,8 @@ def generate_one_pronoun(
                                 # Model Logits
                                 model_logits = mp.get_raw_logits(prompt=prompt).cpu()
                                 pronoun_idxs = mp.get_token_ids(options=pronouns[1])
+                                print(f"pronoun_idxs: {pronoun_idxs}")
+                                print(f"logits: {model_logits}")
                                 pronoun_logits = model_logits[pronoun_idxs].numpy()
 
                                 inputs, output = mp.get_completion(prompt=prompt, temperature=temperature,
